af8c20a54f9ee3050ffa2a2f9695529bc63b1f5a
sik1j <karkisikij2@gmail.com>
2026-05-12 14:14:03 -0700

    impl: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/personal-apps/",
   plugins: [
     tailwindcss(),
     // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
